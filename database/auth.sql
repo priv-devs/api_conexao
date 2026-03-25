3cc9d564e6a432d6625b059cdfd77a022b55bcb2
DOU - Laboratório 207 <doulab207@edu.ms.senac.br>
2026-03-24 20:58:11 -0400

    fix: tabela login

---
+++ database/auth.sql
@@ -0,0 +1,7 @@
+CREATE TABLE auth (
+    id AUTOINCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    id_user INTEGER, 
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id_user)
+);
--- database/usuario.sql
+++ database/usuario.sql
@@ -1,7 +1,6 @@
 CREATE TABLE usuario (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
     tipo_usuario_id INT NOT NULL,
     FOREIGN KEY (tipo_usuario_id) REFERENCES tipo_usuario(id),
     ativo BOOLEAN NOT NULL DEFAULT TRUE,
