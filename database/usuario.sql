93911ee0432638a8c3fd31d1da7f2dbaf54667a0
guio11221 <sguii5147@gmail.com>
2026-03-24 21:14:32 -0400

    fix: iniciando modulo de gerenciamento de tipo_usuarios

--- database/usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuario (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    tipo_usuario_id INT NOT NULL,
-    FOREIGN KEY (tipo_usuario_id) REFERENCES tipo_usuario(id),
-    ativo BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
